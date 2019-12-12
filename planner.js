ba7f3f27a31e89646da99853424cfc6a7b4823ca
Joey Register <josephregister1258@gmail.com>
2019-12-12 12:57:37 -0500

    storing in localstorage (WIP)

--- planner.js
+++ planner.js
@@ -1,33 +1,25 @@
 var tableCells = document.getElementsByTagName("td")
 var monthPicker = document.getElementById("monthpicker")
-
-for (let index = 0; index < tableCells.length; index++) {
-    tableCells[index].addEventListener("mouseover", function() {
-        tableCells[index].setAttribute("class","table-primary")
-    })
-}
-
-for (let index = 0; index < tableCells.length; index++) {
-    tableCells[index].addEventListener("mouseout", function() {
-        tableCells[index].removeAttribute("class", "table-primary")
-    })
-}
-
-
-
 var dropdownMonthMenu = document.getElementById("dropdownmonthmenu")
 var dropdownYearMenu = document.getElementById("dropdownyearmenu")
+var chosenMonth = document.querySelector("#chosenMonth")
+var chosenYear = document.querySelector("#chosenYear")
+var dayHeaders = [document.getElementsByTagName("th")]
+var calCells = document.querySelectorAll("td")
+var years = []
+var dateArray = []
+var firstOfMonth = new Date(dateArray[0])
 
-const years = []
-const yearsArray = () => {
-    const dateStart = moment()
-    const dateEnd = moment().add(10, 'y')
+function yearsArray() {
+    var dateStart = moment()
+    var dateEnd = moment().add(10, 'y')
     while (dateEnd.diff(dateStart, 'years') >= 0) {
       years.push(dateStart.format('YYYY'))
       dateStart.add(1, 'year')
-    }
-}
-const yearDropdownFill = () => {
+    }}
+
+
+function yearDropdownFill() {
        
     for (var i=0;i<years.length;i++) {
     var thisYear = years[i]    
@@ -39,6 +31,7 @@ const yearDropdownFill = () => {
         yearDropdownItem.textContent = thisYear
         dropdownYearMenu.appendChild(yearDropdownItem)
    }}
+
 
 function monthDropdownFill() {
     var months = ["January","February","March","April","May","June","July","August","September","October","November","December"]
@@ -52,74 +45,60 @@ function monthDropdownFill() {
         dropdownItem.setAttribute("name", "month")
         dropdownItem.textContent = thisMonth
         dropdownMonthMenu.appendChild(dropdownItem)
-    }
-}
+    }}
 
-
-
-
-var dayHeaders = [document.getElementsByTagName("th")]
-var calCells = document.querySelectorAll("td")
 
 function createCalendar() {
 for (var i=0;i<calCells.length;i++) {
     if (calendar.firstDay===i) {
         for (var j=0;j<dateArray.length;j++) {
-            
             calCells[i].textContent = dateArray[j]
-            console.log("hey")
+            calCells[i].setAttribute("value",dateArray[j])
             i++
-        }
-        
-    }
-    }}
+        }}}}
+
+function getDates(startDate,stopDate) {
+        var currentDate = moment(startDate);
+        var stopDate = moment(stopDate);
+        while (currentDate <= stopDate) {
+            dateArray.push( moment(currentDate).format('MMMM DD, YYYY') )
+            currentDate = moment(currentDate).add(1, 'days');
+        }}
+
+function clearCal() {
+    $("td").empty()
+    
+}
+
+for (let index = 0; index < tableCells.length; index++) {
+    tableCells[index].addEventListener("mouseover", function() {
+        tableCells[index].setAttribute("class","table-primary")
+    })}
+
+for (let index = 0; index < tableCells.length; index++) {
+    tableCells[index].addEventListener("mouseout", function() {
+        tableCells[index].removeAttribute("class", "table-primary")
+    })}
 
 
-
-
-
-monthDropdownFill()
-yearsArray()
-yearDropdownFill()
-
-
-
-var chosenMonth = document.querySelector("#chosenMonth")
-var chosenYear = document.querySelector("#chosenYear")
-
-
-
-        dropdownMonthMenu.addEventListener("click", function(e){
+dropdownMonthMenu.addEventListener("click", function(e){
             var selectedMonth = e.target.textContent
             chosenMonth.textContent = selectedMonth
         })
     
 
-
-        dropdownYearMenu.addEventListener("click", function(e){
-           
+dropdownYearMenu.addEventListener("click", function(e){
             var selectedYear = e.target.textContent
             chosenYear.textContent = selectedYear
         })
   
-var dateArray = []
 
-
-function getDates(startDate,stopDate) {
-    var currentDate = moment(startDate);
-    var stopDate = moment(stopDate);
-    while (currentDate <= stopDate) {
-        dateArray.push( moment(currentDate).format('MMMM DD, YYYY') )
-        currentDate = moment(currentDate).add(1, 'days');
-    }
-    console.log(dateArray);
-}
-    
 var calendar = new Object()     
             calendar.clock = setInterval(function() { 
                 if (chosenYear.textContent!==""&&chosenMonth.textContent!=="") {
                     calendar.combinedCalendar = chosenMonth.textContent+" "+chosenYear.textContent;
-                    
+                    dateArray = []
+                    clearCal()
                     calendar.dt = new Date(calendar.combinedCalendar);
                     // GET THE MONTH AND YEAR OF THE SELECTED DATE.
                     calendar.month = calendar.dt.getMonth();
@@ -129,15 +108,39 @@ var calendar = new Object()
                     calendar.FirstDay = new Date(calendar.year, calendar.month, 1);
                     calendar.LastDay = new Date(calendar.year, calendar.month + 1, 0);
                     getDates(calendar.FirstDay, calendar.LastDay)
-                    calendar.firstDay = calendar.FirstDay.getDay(firstOfMonth)
-                    console.log(calendar.firstDay)
-                    console.log(calendar.FirstDay,calendar.LastDay)
-                    createCalendar()    
-                    clearInterval(calendar.clock)
+                    calendar.firstDay = calendar.FirstDay.getDay(firstOfMonth)   
+                    createCalendar()
+                    chosenMonth.textContent = ""
+                    chosenYear.textContent = ""
+                    }},1000)
+
+
+monthDropdownFill()
+yearsArray()
+yearDropdownFill()
+
+$("td").attr("data-target","#modal")
+$("td").attr("data-toggle","modal")
+
+$("#modal").on("shown.bs.modal", function (event) {
+    $('#myInput').trigger('focus')
+    alert(localStorage.getItem(title))
+    var button = $(event.relatedTarget); // Button that triggered the modal 
+    var modal  = $(this);
+    var title = button.attr("value")
+    modal.find('.modal-title').text(title)
+})
+
+$('#modal').on('hidden.bs.modal', function (event) {
+    event.preventDefault()
+    var modalT = document.querySelector("#modalLabel")
+    var title = modalT.textContent
+    var modalObj = {
+        time : document.querySelectorAll(".col-form-label").values(),
+        event : document.querySelectorAll(".form-control").values()
+    }
+
+    localStorage.setItem(title, JSON.stringify(modalObj))
+    // do something...
+  })
                         
-                    }
-                     return
-                        },1000)
-
-
-                        var firstOfMonth = new Date(dateArray[0])
